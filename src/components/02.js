6e68ccae5130260544df8713082cf86bd0daeaf5
HuyTrinh <dinhhuyams@gmail.com>
2019-03-28 04:42:48 +0200

    Add custom hook example solution@

--- src/components/02.js
+++ src/components/02.js
@@ -18,3 +18,40 @@ export default Counter
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The "use" prefix is a convention
+// const useCounter = () => {
+//   const [count, setCount] = useState(0);
+//
+//   const incrementCount = () => setCount(count + 1)
+//
+//   return [count, incrementCount];
+// }
+//
+// const Counter = () => {
+//
+//   const [count, incrementCount] = useState(0);
+//
+//   return (
+//       <Button onClick={incrementCount}>{count}</Button>
+//   )
+// }
+//
+
