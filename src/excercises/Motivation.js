26850c6a0805ee2face90501f3f6f27691bcb2f4
HuyTrinh <huy@makehelsinki.com>
2019-03-29 10:21:21 +0200

    Update motivation

--- src/excercises/Motivation.js
+++ src/excercises/Motivation.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
+import {Card} from 'react-bootstrap';
+import './Motivation.css';
 
 class Motivation extends Component {
   render() {
     return (
-        <div className="Home">
-          <h1>Why?</h1>
-          <ul>
-            <li>Javascript</li>
-            <li>Lifecycle</li>
-            <li> Reuse stateful logic</li>
-          </ul>
+        <div className="motivation">
+          <div>
+            <h1>Why?</h1>
+            <ul>
+              <li>Javascript</li>
+              <li>Lifecycle</li>
+              <li> Reuse stateful logic</li>
+            </ul>
+          </div>
+          <div>
+            <h1>What is Hook?</h1>
+            <Card body>Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.</Card>
+          </div>
         </div>
     );
   }
